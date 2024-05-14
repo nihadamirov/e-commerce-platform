249e1cf57912a696d6fe8142590c10c368d36d69
Nihad <nihadamirov98@gmail.com>
2024-05-14 11:43:31 +0400

    first commit

---
+++ src/main/java/com/euphoria_ecommerce/EuphoriaEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.euphoria_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuphoriaEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EuphoriaEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/euphoria_ecommerce/EuphoriaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.euphoria_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuphoriaEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
